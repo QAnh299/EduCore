cde24ee44cb274d32ba51bcb46f9f353bcf8eec8
trang <trangnth15904@gmail.com>
2026-05-12 14:03:53 +0700

    _

--- resources/views/components/sidebar-teacher-config.blade.php
+++ resources/views/components/sidebar-teacher-config.blade.php
@@ -78,7 +78,13 @@
             'icon' => 'bi bi-bar-chart',
             'visible' => true,
         ],
-        
+        [
+            'key' => 'evaluations-report',
+            'label' => __('general.evaluation_report'),
+            'route' => route('teacher.evaluations.report'),
+            'icon' => 'bi bi-star',
+            'visible' => true,
+        ],
     ];
 @endphp
 
